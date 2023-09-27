1927abe25ea19d517652b0911042f4c5df5c22cd
Ricardo Fernández Guzmán <ricardofg2000@gmail.comm>
2023-09-28 01:27:27 +0200

    Borrado imports sin uso

--- src/main/java/com/CreacionAsig.java
+++ src/main/java/com/CreacionAsig.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
